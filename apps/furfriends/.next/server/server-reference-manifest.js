ffd60b4c32352c8a7973b7f91251347c822fc414
Catherine Cabrera <catheria12@gmail.com>
2024-09-12 18:31:19 -0400

    Remove .next directories from version control

--- apps/furfriends/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"4G9EX1qehZTQd0lqJMBKQX6lNdZGFHTBrM4ARLe876I=\"\n}"
--- apps/portfolio/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"L3hgXw1tcWhRXTljwXgqJoCbAhOpEeHDR54FWmK7BdY=\"\n}"
